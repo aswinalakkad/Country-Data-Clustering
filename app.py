1cdb3b0b901b1687cf862fe5c63c529c2ffafb6a
aswinalakkad <aswinalakkad00@gmail.com>
2026-01-24 12:52:00 +0530

    Change input fields to text inputs for predictions
    Updated input method for user data collection in the main function.

--- app.py
+++ app.py
@@ -27,30 +27,30 @@ def prediction(input_data):
         return 'Under Developed'
 
 def main():
+    
     st.title('Help International Foundation')
+    
     st.subheader('This application will give the status of the country based on socio-economic factors')
 
-    with st.form("country_form"):
-        ch_mort = st.number_input('Child mortality rate', min_value=0.0)
-        exp = st.number_input('Exports (% GDP)', min_value=0.0)
-        imp = st.number_input('Imports (% GDP)', min_value=0.0)
-        hel = st.number_input('Health expenditure (% GDP)', min_value=0.0)
-        inc = st.number_input('Average income per person', min_value=0.0)
-        inf = st.number_input('Inflation rate')
-        life_exp = st.number_input('Life expectancy', min_value=0.0)
-        total_fert = st.number_input('Total fertility rate', min_value=0.0)
-        gdpp = st.number_input('GDP per capita', min_value=0.0)
+    ch_mort = st.text_input('Enter the child mortality rate:')
+    exp = st.text_input('Enter Exports (% GDP):')
+    imp = st.text_input('Enter Imports (% GDP):')
+    hel = st.text_input('Enter Expentiture on health (% GDP):')
+    inc = st.text_input('Enter average income per person')
+    inf = st.text_input('Enter Inflation rate')
+    life_exp = st.text_input('Enter Life Expectency')
+    total_fert = st.text_input('Enter number of children that would be born to each woman')
+    gdpp = st.text_input('Enter The GDP per capita')
 
-        submit = st.form_submit_button("Predict")
+    # save all the input in a 2d list 
+    input_list = [[ch_mort, exp, imp, hel, inc, inf, life_exp, total_fert, gdpp]]
 
-    if submit:
-        input_list = [[
-            ch_mort, exp, imp, hel, inc,
-            inf, life_exp, total_fert, gdpp
-        ]]
+    # create a button to predict 
+    if st.button('Predict'):
         response = prediction(input_list)
         st.success(response)
 
 # to execute the main function 
 if __name__ == '__main__':
     main()
+
