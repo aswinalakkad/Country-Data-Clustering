069b706cf4a1cdcebe14873034f2a46c52e948c6
aswinalakkad <aswinalakkad00@gmail.com>
2026-01-24 12:46:45 +0530

    Refactor input handling using Streamlit forms

--- app.py
+++ app.py
@@ -1,67 +1,24 @@
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-import pickle 
-
-#Load the instances that were created 
-
-with open('final_model.pkl','rb') as file:
-    model = pickle.load(file)
-    
-with open('pca.pkl','rb') as file:
-    pca = pickle.load(file)
-    
-with open('scaler.pkl','rb') as file:
-    scaler = pickle.load(file)   
-
-def prediction(input_data):
-    
-    scaled_data = scaler.transform(input_data)
-
-    pca_data = pca.transform(scaled_data)
-
-    pred=model.predict(pca_data)[0]
-
-    if pred == 0:
-        return 'Developing'
-    elif pred == 1:
-        return 'Developed'
-    else:
-        return 'Under Developed'
-
 def main():
-    
     st.title('Help International Foundation')
-    
     st.subheader('This application will give the status of the country based on socio-economic factors')
 
-    ch_mort = st.text_input('Enter the child mortality rate:')
+    with st.form("country_form"):
+        ch_mort = st.number_input('Child mortality rate', min_value=0.0)
+        exp = st.number_input('Exports (% GDP)', min_value=0.0)
+        imp = st.number_input('Imports (% GDP)', min_value=0.0)
+        hel = st.number_input('Health expenditure (% GDP)', min_value=0.0)
+        inc = st.number_input('Average income per person', min_value=0.0)
+        inf = st.number_input('Inflation rate')
+        life_exp = st.number_input('Life expectancy', min_value=0.0)
+        total_fert = st.number_input('Total fertility rate', min_value=0.0)
+        gdpp = st.number_input('GDP per capita', min_value=0.0)
 
-    exp = st.text_input('Enter Exports (% GDP):')
+        submit = st.form_submit_button("Predict")
 
-    imp = st.text_input('Enter Imports (% GDP):')
-    
-    hel = st.text_input('Enter Expentiture on health (% GDP):')
-    
-    inc = st.text_input('Enter average income per person')
-    
-    inf =  st.text_input('Enter Inflation rate')
-    
-    life_exp = st.text_input('Enter Life Expectency')
-    
-    total_fert = st.text_input('Enter number of children that would be born to each woman')
-    
-    gdpp = st.text_input('Enter The GDP per capita')
-
-    #save all the input in a 2d list 
-    input_list = [[ch_mort,exp,imp,hel,inc,inf,life_exp,total_fert,gdpp]]
-
-    #create a button to predict 
-    if st.button('Predict'):
+    if submit:
+        input_list = [[
+            ch_mort, exp, imp, hel, inc,
+            inf, life_exp, total_fert, gdpp
+        ]]
         response = prediction(input_list)
         st.success(response)
-#to execute the main function 
-if __name__ == '__main__':
-    main()
-
